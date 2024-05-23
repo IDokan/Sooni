58be730db767158d85d1a36849bb3714ab5d7f20
Sinil <rtd99062@gmail.com>
2024-05-23 13:52:07 +0900

    Implemented rough version of MoveByClicking.
    Implemented set position in _process function.
    
    _input function does not be called in every frames. Thus, I implemented drag logics in a _process function.
    
    Followed below tutorials:
    1. https://forum.godotengine.org/t/how-to-define-input-method-in-gdnative-c/21424/2
    2. https://www.youtube.com/watch?v=uhgswVkYp0o
    3. https://www.youtube.com/watch?v=8cV-5ByZLOE

--- demo/tutorial.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-
-func _input(event):
- if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-   print("You clicked on Sprite!")
--- src/MoveByClick.cpp
+++ src/MoveByClick.cpp
@@ -1,11 +1,12 @@
 #include "MoveByClick.h"
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/classes/input.hpp>
 #include <godot_cpp/classes/input_event_mouse_button.hpp>
 
 using namespace godot;
 
 void MoveByClick::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("_input", "event"), &MoveByClick::_input);
 }
 
 MoveByClick::MoveByClick() {
@@ -18,22 +19,33 @@ MoveByClick::~MoveByClick() {
 
 void MoveByClick::_process(double delta) {
 	(delta);
-}
 
-// Should I use godot::Ref<godot::InputEvent>?
-void MoveByClick::_input(Ref<InputEvent> event)
-{
-                set_position(Vector2(0, 0));
-    InputEventMouseButton* mouseEvent = dynamic_cast<InputEventMouseButton*>(event.ptr());
-
-    // if(mouseEvent != nullptr)
+    Input* inputEvent = Input::get_singleton();
+    if(inputEvent->is_mouse_button_pressed(MOUSE_BUTTON_LEFT))
     {
-        // if(mouseEvent->is_pressed() && mouseEvent->get_button_index() == MOUSE_BUTTON_LEFT)
+        if(get_rect().has_point(to_local(get_global_mouse_position())))
         {
-            //if(get_rect().has_point(to_local(mouseEvent->get_position())))
-            {
-                set_position(Vector2(0, 0));
-            }
+            set_position(get_global_mouse_position());
+            
         }
     }
+
+}
+
+void MoveByClick::_input(Ref<InputEvent> event)
+{
+                
+    // InputEventMouseButton* mouseEvent = dynamic_cast<InputEventMouseButton*>(event.ptr());
+
+    // if(mouseEvent != nullptr)
+    // {
+    //     if(mouseEvent->is_pressed() && mouseEvent->get_button_index() == MOUSE_BUTTON_LEFT)
+    //     {
+    //         if(get_rect().has_point(to_local(mouseEvent->get_position())))
+    //         {
+    //             // set_position(mouseEvent->get_position());
+    //             UtilityFunctions::print(mouseEvent->get_position());
+    //         }
+    //     }
+    // }
 }
