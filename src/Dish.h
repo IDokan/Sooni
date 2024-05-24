701f1126db1bb9ab726f4398ebe4e051abd64abb
Sinil <rtd99062@gmail.com>
2024-05-24 11:54:48 +0900

    Apply rigid body physics on a dish.
    Use _ready() for a purpose of initialization. A logic that changes position of rigid body should be in _integrate_forces rather than _process, because _process cannot affect rigid body physics.

---
+++ src/Dish.h
@@ -0,0 +1,37 @@
+#ifndef DISH_H
+#define DISH_H
+
+#include <godot_cpp/classes/rigid_body2d.hpp>
+#include <godot_cpp/classes/collision_shape2d.hpp>
+#include <godot_cpp/classes/input_event.hpp>
+#include <godot_cpp/classes/physics_direct_body_state2d.hpp>
+
+namespace godot {
+
+class InputEvent;
+class CollisionShape2D;
+class PhysicsDirectBodyState2D;
+
+class Dish : public RigidBody2D {
+	GDCLASS(Dish, RigidBody2D)
+
+protected:
+	static void _bind_methods();
+
+public:
+	Dish();
+	~Dish();
+
+	void _ready();
+	void _process(double delta) override;
+	void _integrate_forces(PhysicsDirectBodyState2D *state);
+
+    void _input(Ref<InputEvent> event);
+
+private:
+	CollisionShape2D* collision_shape;
+};
+
+}
+
+#endif
