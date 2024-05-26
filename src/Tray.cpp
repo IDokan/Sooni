5a9412671c4f76b65ba7d1b25fe5049dd14cca06
Sinil <rtd99062@gmail.com>
2024-05-26 20:27:23 +0900

    #1 Add essential components to detect collisions.
    Area2D.gd - A test Godot script for collision detecting. It will be removed after finishing implementing tray.
    main.tscn - Add a tray object on a scene.
    Tray - Added area components for detecting foods. Set up in _ready function.

--- src/Tray.cpp
+++ src/Tray.cpp
@@ -3,6 +3,7 @@
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/classes/physics_material.hpp>
 #include <godot_cpp/classes/collision_shape2d.hpp>
+#include <godot_cpp/classes/area2d.hpp>
 #include <godot_cpp/classes/sprite2d.hpp>
 
 using namespace godot;
@@ -15,9 +16,11 @@ void godot::Tray::_bind_methods()
 }
 
 godot::Tray::Tray()
-    : sprite(memnew(Sprite2D)), rect_shape(memnew(RectangleShape2D))
+    : body_collision(memnew(CollisionShape2D)), body_collision_shape(memnew(RectangleShape2D)),
+        sprite(memnew(Sprite2D)),
+        area(memnew(Area2D)), area_collision(memnew(CollisionShape2D)), area_collision_shape(memnew(RectangleShape2D))
 {
-    collision_shape = memnew(CollisionShape2D);
+    
 }
 
 godot::Tray::~Tray()
@@ -31,11 +34,23 @@ void godot::Tray::_ready()
     add_child(sprite);
     sprite->set_owner(this);
 
-    rect_shape->set_size(sprite->get_rect().size);
-    collision_shape->set_name("Collision Shape");
-    collision_shape->set_shape(rect_shape);
-    add_child(collision_shape);
-    collision_shape->set_owner(this);
+    const Vector2 sprite_size = sprite->get_rect().size;
+    body_collision_shape->set_size(sprite_size);
+    body_collision->set_name("Collision Shape");
+    body_collision->set_shape(body_collision_shape);
+    add_child(body_collision);
+    body_collision->set_owner(this);
+
+
+    // Hierarchy: Body -> area -> area collision -> area collision rectangle shape
+    area_collision_shape->set_size(Vector2(sprite_size.x, sprite_size.y * TRAY_VERTICAL_DETECT_RANGE));
+    area_collision->set_name("Area Collision Shape");
+    area_collision->set_shape(area_collision_shape);
+    area->set_name("Area");
+    area->add_child(area_collision);
+    area_collision->set_owner(area);
+    add_child(area);
+    area->set_owner(this);
 }
 
 void godot::Tray::_process(double delta)
@@ -52,5 +67,7 @@ void godot::Tray::set_texture(const Ref<Texture> &_texture)
     texture = _texture;
     sprite->set_texture(texture);
 
-    rect_shape->set_size(sprite->get_rect().size);
+    Vector2 sprite_size = sprite->get_rect().size;
+    body_collision_shape->set_size(sprite_size);
+    area_collision_shape->set_size(Vector2(sprite_size.x, sprite_size.y * TRAY_VERTICAL_DETECT_RANGE));
 }
