f0420d2c6fa9286de353baafa2cdb9fd356d7d20
Sinil <rtd99062@gmail.com>
2024-07-04 23:17:22 +0900

    Created source files for stove.
    Stove - A base class for stove.

---
+++ src/Stove.h
@@ -0,0 +1,9 @@
+/******************************************************************************
+Copyright (C)
+File Name:   Stove.h
+Author
+	- sinil.kang	rtd99062@gmail.com
+Creation Date: 7.4.2024
+	Header file for a stove.
+    It is a work-table to create a dish.
+******************************************************************************/
